7781f38549fdfd42a6204ab29b48ecb7c921cec7
ArthurVinog@gmail.com <ArthurVinog@gmail.com@abc83770-d963-1ce9-5223-486c1e84b270>
2013-07-23 04:16:18 +0000

    Create the account flow, configure Spring Security, protect account flow and create Spring Security DAO authentication

--- trunk/j2eeapplication/src/main/java/com/example/j2eeapp/services/impl/UserServiceImpl.java
+++ trunk/j2eeapplication/src/main/java/com/example/j2eeapp/services/impl/UserServiceImpl.java
@@ -1,5 +1,15 @@
 package com.example.j2eeapp.services.impl;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
 import com.example.j2eeapp.dao.UserDao;
 import com.example.j2eeapp.domain.UserEntity;
 import com.example.j2eeapp.services.UserService;
@@ -9,7 +19,7 @@ import com.example.j2eeapp.services.UserService;
  * 
  * @author Arthur Vin
  */
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserService, UserDetailsService {
 
 	private UserDao userDao;
 	
@@ -22,6 +32,25 @@ public class UserServiceImpl implements UserService {
 	public boolean createUser(UserEntity userEntity) {
 		userDao.save(userEntity);
 		return true;
+	}
+	
+	/**
+	 * Construct UserDetails instance required by spring security
+	 */
+	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+		
+		UserEntity user = userDao.loadUserByUserName(userName);
+		
+		if (user == null) {
+			throw new UsernameNotFoundException(String.format("No such user with name provided '%s'", userName));
+		}
+		
+		Collection<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+		authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+		
+		User userDetails = new User(user.getUserName(), user.getPassword(), authorities);
+		
+		return userDetails;
 	}
 
 	public UserDao getUserDao() {
