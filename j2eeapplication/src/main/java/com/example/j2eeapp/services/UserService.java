375c705ce830ff4435584c60c0c30c1c65d7a0e2
ArthurVinog <ArthurVinog@gmail.com>
2013-07-25 05:47:42 +0000

    Implementing ajax database querying, reading properties files, etc...

--- j2eeapplication/src/main/java/com/example/j2eeapp/services/UserService.java
+++ j2eeapplication/src/main/java/com/example/j2eeapp/services/UserService.java
@@ -1,5 +1,7 @@
 package com.example.j2eeapp.services;
 
+import javax.faces.event.AjaxBehaviorEvent;
+
 import com.example.j2eeapp.domain.UserEntity;
 
 /**
@@ -16,4 +18,20 @@ public interface UserService {
 	 * @return true if success
 	 */
 	boolean createUser(UserEntity userEntity);
+	
+	/**
+	 * Check user name availability. UI ajax use.
+	 * 
+	 * @param ajax event
+	 * @return
+	 */
+	boolean checkAvailable(AjaxBehaviorEvent event);
+	
+	/**
+	 * Retrieves full User record from database by user name
+	 * 
+	 * @param userName
+	 * @return UserEntity
+	 */
+	UserEntity loadUserEntityByUsername(String userName);
 }
